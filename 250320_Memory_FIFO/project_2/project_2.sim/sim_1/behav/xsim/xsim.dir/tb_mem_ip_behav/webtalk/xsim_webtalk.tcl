1e390cb62fb636ef5409549d294ef6c487988c11
migrell <parkjiho333@naver.com>
2025-03-20 15:51:49 +0900

    왼료

--- 250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/xsim_webtalk.tcl
+++ 250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Mar 20 13:00:23 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Mar 20 14:14:02 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "449354ba-6eca-429a-939e-5369ea55f2e4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "b2962164025d4a23b36777205434c571" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "11" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "12th Gen Intel(R) Core(TM) i7-1260P" -context "user_environment"
@@ -23,13 +23,11 @@ webtalk_add_data -client project -key total_processors -value "1" -context "user
 webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1624754905 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "195762695 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "16.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "15612_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 769279648 -regid "" -xml C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.78_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "15608_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 329004104 -regid "" -xml C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kccistc/Desktop/250320_Memory_FIFO/250320_Memory_FIFO/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/tb_mem_ip_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
